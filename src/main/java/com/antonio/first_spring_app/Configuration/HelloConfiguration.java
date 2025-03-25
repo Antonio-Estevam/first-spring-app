7b0289506ac79fb97ce15ed7d1f0c45d3b00f86e
antonio-estevam <jhonnyimmbe@gmail.com>
2025-03-25 14:37:15 +0000

    first part video

---
+++ src/main/java/com/antonio/first_spring_app/Configuration/HelloConfiguration.java
@@ -0,0 +1,10 @@
+package com.antonio.first_spring_app.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloConfiguration {
+    @Bean
+
+}
